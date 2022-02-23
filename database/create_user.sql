bb8e6f9732aac68277907deeb482be522bf53150
nikolas-virionis <nikolas.virionis@sptech.school>
2022-02-23 18:45:01 -0300

    script de criação de usuario para utilização

---
+++ database/create_user.sql
@@ -0,0 +1,9 @@
+DROP USER IF EXISTS 'homebox' @'localhost';
+
+DROP USER IF EXISTS 'homebox' @'%';
+
+CREATE USER IF NOT EXISTS 'homebox' @'%' IDENTIFIED BY 'Homebox265@';
+
+GRANT ALL PRIVILEGES ON `homebox`.* TO `homebox` @`%`;
+
+FLUSH PRIVILEGES;
