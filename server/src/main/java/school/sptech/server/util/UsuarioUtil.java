51f047573e33c8817891196ffbb9323852ad6353
Leo <leoigornunes@protonmail.com>
2022-03-13 15:38:00 -0300

    Adicionando Classes excluidas

---
+++ server/src/main/java/school/sptech/server/util/UsuarioUtil.java
@@ -0,0 +1,22 @@
+package school.sptech.server.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class UsuarioUtil {
+
+    public static String getToken() {
+        String caracteres = "qwertyuiopasdfghjklçzxcvbnm1234567890".repeat(3);
+
+        List<String> letras = Arrays.asList(caracteres.split(""));
+
+        Collections.shuffle(letras);
+
+        String token = letras.stream().reduce("", String::concat);
+
+        return token.substring(0, 16);
+    }
+
+    // necessaria a verificação de se aquele token ja é o utilizado pelo usuario (chances baixas mas nunca se sabe)
+}
