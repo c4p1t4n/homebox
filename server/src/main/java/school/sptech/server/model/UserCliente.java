96189fe1576b7aa6b63b89e4c5adc71e6d6730e8
Leo <leoigornunes@protonmail.com>
2022-03-13 15:19:51 -0300

    Adicionando Classe UserCliente

---
+++ server/src/main/java/school/sptech/server/model/UserCliente.java
@@ -0,0 +1,26 @@
+package school.sptech.server.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@Entity
+@Table(name="usuario")
+public class UserCliente extends User {
+
+    public UserCliente(){
+
+    }
+
+    @Override
+    public String login(String user,String password) {
+        String stringReturn = "Email e/ou senha incorreta";
+        if(getSenha().equals(password) & getEmail().equals(user) ){
+            stringReturn = String.format("Usuario Prestador logado com sucesso");
+            return  stringReturn;
+        }
+        return stringReturn;
+    }
+
+    public UserCliente(Integer id, String nome, String email, String senha, String cpf, String token, String tipoCliente, String foto) {
+        super(id, nome, email, senha, cpf, token, tipoCliente, foto);
+    }
+}
