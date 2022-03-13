871d586f30f783c676a104a6e7956193ccb26260
nikolas-virionis <nikolas.virionis@sptech.school>
2022-03-13 20:34:22 -0300

    implementação da interface ILogin e correções gerais

--- server/src/main/java/school/sptech/server/model/UserCliente.java
+++ server/src/main/java/school/sptech/server/model/UserCliente.java
@@ -2,25 +2,28 @@ package school.sptech.server.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 public class UserCliente extends User {
 
-    public UserCliente(){
+    public UserCliente() {
 
     }
 
     @Override
-    public String login(String user,String password) {
+    public String login(String user, String password) {
         String stringReturn = "Email e/ou senha incorreta";
-        if(getSenha().equals(password) & getEmail().equals(user) ){
+        if (getSenha().equals(password) & getEmail().equals(user)) {
             stringReturn = String.format("Usuario Prestador logado com sucesso");
-            return  stringReturn;
+            setAutenticado(Boolean.TRUE);
+            return stringReturn;
         }
         return stringReturn;
     }
 
-    public UserCliente(Integer id, String nome, String email, String senha, String cpf, String token, String tipoCliente, String foto) {
-        super(id, nome, email, senha, cpf, token, tipoCliente, foto);
+    public UserCliente(Integer id, String nome, String email, String senha, String cpf, String token, String tipo,
+            String foto, String cep) {
+        super(id, nome, email, senha, cpf, token, tipo, foto, cep);
     }
 }
--- server/src/main/java/school/sptech/server/model/UserPrestador.java
+++ server/src/main/java/school/sptech/server/model/UserPrestador.java
@@ -4,22 +4,25 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 public class UserPrestador extends User {
     public UserPrestador() {
     }
 
     @Override
-    public String login(String user,String password) {
+    public String login(String user, String password) {
         String stringReturn = "Email e/ou senha incorreta";
-        if(getSenha().equals(password) & getEmail().equals(user) ){
+        if (getSenha().equals(password) & getEmail().equals(user)) {
+
+            setAutenticado(Boolean.TRUE);
             stringReturn = String.format("Usuario Prestador logado com sucesso");
-            return  stringReturn;
+            return stringReturn;
         }
         return stringReturn;
     }
 
-    public UserPrestador(Integer id, String nome, String email, String senha, String cpf, String token, String tipoCliente, String foto) {
-        super(id, nome, email, senha, cpf, token, tipoCliente, foto);
+    public UserPrestador(Integer id, String nome, String email, String senha, String cpf, String token,
+            String tipo, String foto, String cep) {
+        super(id, nome, email, senha, cpf, token, tipo, foto, cep);
     }
 }
