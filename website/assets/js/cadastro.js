334757adae1c0fcf32089a25b3f282d060825d83
Rodrigo Garcez <rodrigo.hermann@sptech.school>
2022-03-14 12:49:26 -0300

    correção caminho logos cadastro

--- website/assets/js/cadastro.js
+++ website/assets/js/cadastro.js
@@ -1,7 +1,7 @@
 const btnChangeForm = document.querySelector("[changeForm]");
 const cadastro = document.querySelector("[cadastro]");
 
-changeForm.addEventListener("click", e => {
+btnChangeForm.addEventListener("click", e => {
     document.getElementById("form_1").style.display = "none";
     document.getElementById("form_2").style.display = "";
 });
