73a0836ecc364261b30dfd195dffcf202c2b6b28
Rodrigo Garcez <rodrigo.hermann@sptech.school>
2022-03-16 21:02:58 -0300

    Arrumando js do homeLogado

--- website/assets/js/homeLogado.js
+++ website/assets/js/homeLogado.js
@@ -2,5 +2,5 @@ const openLogoff = document.querySelector("[openLogoff]");
 const logoff = document.querySelector("[logoff]");
 
 openLogoff.addEventListener("click", e => {
-    logoff.style.display == "block" ? "none" : "block";
+    logoff.style.display = logoff.style.display == "block" ? "none" : "block";
 });
