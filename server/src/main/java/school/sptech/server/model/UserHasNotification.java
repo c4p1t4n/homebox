7d4f0ddfe9e4a8ecf5e997521236fc75c55bae5b
nikolas-virionis <nikolas.virionis@sptech.school>
2022-04-09 11:41:20 -0300

    Criação das models para serem usadas na APi de notificação

---
+++ server/src/main/java/school/sptech/server/model/UserHasNotification.java
@@ -0,0 +1,70 @@
+package school.sptech.server.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+import school.sptech.server.service.UserHasNotificationId;
+
+@Entity
+@Table(name = "user_has_notification")
+@IdClass(UserHasNotificationId.class)
+public class UserHasNotification {
+    @Id
+    @Column(name = "fk_user")
+    private Integer fkUser;
+
+    @Id
+    @Column(name = "fk_notification")
+    private Integer fkNotification;
+
+    @Column(name = "notification_date")
+    private LocalDate notificationDate;
+
+    @Column(name = "seen")
+    private char seen;
+
+    public UserHasNotification(Integer fkUser, Integer fkNotification, LocalDate notificationDate,
+            char seen) {
+        this.fkUser = fkUser;
+        this.fkNotification = fkNotification;
+        this.notificationDate = notificationDate;
+        this.seen = seen;
+    }
+
+    public UserHasNotification() {
+    }
+
+    public Integer getFkUser() {
+        return fkUser;
+    }
+
+    public void setFkUser(Integer fkUser) {
+        this.fkUser = fkUser;
+    }
+
+    public Integer getFkNotification() {
+        return fkNotification;
+    }
+
+    public void setFkNotification(Integer fkNotification) {
+        this.fkNotification = fkNotification;
+    }
+
+    public LocalDate getNotificationDate() {
+        return notificationDate;
+    }
+
+    public void setNotificationDate(LocalDate notificationDate) {
+        this.notificationDate = notificationDate;
+    }
+
+    public char getSeen() {
+        return seen;
+    }
+
+    public void setSeen(char seen) {
+        this.seen = seen;
+    }
+
+}
