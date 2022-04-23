ae965f61b4d6a3df42cb982f1aca32665b5dae5f
analuizacfreitas <ana.freitas@bandtec.com.br>
2022-04-23 12:20:50 -0300

    Remove import desnecessário

--- server/src/main/java/school/sptech/server/controllers/CategoryController.java
+++ server/src/main/java/school/sptech/server/controllers/CategoryController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.sptech.server.model.Category;
-import school.sptech.server.model.Notification;
 import school.sptech.server.repository.CategoryRepository;
 
 
