389bf9368a89a30808569bfc59b48eb1bdfff6c9
analuizacfreitas <ana.freitas@bandtec.com.br>
2022-04-27 17:50:38 -0300

    Adiciona algoritmo de ordenação (bubble sort)

--- server/src/main/java/school/sptech/server/csv/UserWorkerTest.java
+++ server/src/main/java/school/sptech/server/csv/UserWorkerTest.java
@@ -4,6 +4,7 @@ import school.sptech.server.model.UserWorker;
 import school.sptech.server.service.ListObj;
 
 public class UserWorkerTest {
+
     public static void main(String[] args) {
 
         ListObj<UserWorker> userWorkerList = new ListObj(5);
@@ -14,12 +15,12 @@ public class UserWorkerTest {
                                           "00011122244", "tokenExemplo2", "worker", "foto.png",
                                           "1234577");
         UserWorker user3 = new UserWorker(3, "Felipe Abreu", "felipe@gmail.com", "senhaExemplo3",
-                                          "00011122244", "tokenExemplo3", "worker", "foto.png",
+                                          "00011122255", "tokenExemplo3", "worker", "foto.png",
                                           "1234588");
 
         userWorkerList.add(user1);
-        userWorkerList.add(user2);
         userWorkerList.add(user3);
+        userWorkerList.add(user2);
 
 //        System.out.printf("%5s %-14s %-20s %-14s %-14s %-14s %-10s %-14s %-14s\n",
 //                          "ID", "NOME", "EMAIL", "SENHA", "CPF", "TOKEN", "TIPO", "FOTO", "CEP");
@@ -27,5 +28,30 @@ public class UserWorkerTest {
 
         UserWorkerCsv.recordCsvFile(userWorkerList, "userWorker");
         UserWorkerCsv.readCsvFile("userWorker");
+
+        System.out.println("--- Exibindo antes do bubble sort ---");
+        userWorkerList.show();
+
+        bubbleSort(userWorkerList);
+
+        System.out.println();
+        System.out.println("--- Exibindo após o bubble sort ---");
+        userWorkerList.show();
+    }
+
+    public static void bubbleSort(ListObj<UserWorker> v) {
+
+        int aux = 0;
+
+        for (int i = 0; i < v.getSize() - 1; i++){
+            for (int j = 1; j < v.getSize() - i; j++){
+                if (v.getElement(j-1).getId() > v.getElement(j).getId()){
+                    aux = v.getElement(j).getId();
+                    v.getElement(j).setId(v.getElement(j-1).getId());
+                    v.getElement(j-1).setId(aux);
+                }
+            }
+        }
+
     }
 }
