cad251c4ea81b8873286316c67a85410ebe4ec86
nikolas-virionis <nikolas.virionis@sptech.school>
2022-05-28 19:59:45 -0300

    hashCode e equals nas keys, papra retirar aviso na inicialização do server

--- server/src/main/java/school/sptech/server/model/keys/ChatHasMsgKey.java
+++ server/src/main/java/school/sptech/server/model/keys/ChatHasMsgKey.java
@@ -21,4 +21,33 @@ public class ChatHasMsgKey implements Serializable {
     ChatHasMsgKey(){
 
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idChat == null) ? 0 : idChat.hashCode());
+        result = prime * result + ((idMsg == null) ? 0 : idMsg.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ChatHasMsgKey other = (ChatHasMsgKey) obj;
+        if (idChat == null) {
+            if (other.idChat != null)
+                return false;
+        } else if (!idChat.equals(other.idChat))
+            return false;
+        if (idMsg == null) {
+            if (other.idMsg != null)
+                return false;
+        } else if (!idMsg.equals(other.idMsg))
+            return false;
+        return true;
+    }
 }
