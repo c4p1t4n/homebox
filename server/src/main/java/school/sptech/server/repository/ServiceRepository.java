5d3f8764af373ba42a06a3e81b9b1c99c573f629
nikolas-virionis <nikolas.virionis@sptech.school>
2022-05-28 20:02:29 -0300

    metodos para pegar dados dos serviços, usuarios e categorias para pesquisa

--- server/src/main/java/school/sptech/server/repository/ServiceRepository.java
+++ server/src/main/java/school/sptech/server/repository/ServiceRepository.java
@@ -3,12 +3,24 @@ package school.sptech.server.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import school.sptech.server.model.Category;
 import school.sptech.server.model.Service;
+import school.sptech.server.model.User;
 
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
     List<Service> findByNameAndWorkerId(String name, Integer idWorker);
 
-    List<Category> findDistinctCategoryByWorkerId(Integer idWorker);
+    @Query("select distinct s.category from Service s where s.worker.id = ?1")
+    List<Category> findDistinctByWorkerId(Integer idWorker);
+
+    @Query("select distinct s.worker from Service s where s.category.name LIKE %:name%")
+    List<User> findByCategoryNameContainsIgnoreCase(String name);
+
+    @Query("select distinct s.worker from Service s where s.worker.name LIKE %:info%")
+    List<User> searchUsers(String info);
+
+    boolean existsByWorkerId(Integer idWorker);
+
 }
