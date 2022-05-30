0267f02f1fe98672a38eb280dcdd23809bb6f646
nikolas-virionis <nikolas.virionis@sptech.school>
2022-05-30 17:18:46 -0300

    métodos para realizar o login do usuario

--- server/src/main/java/school/sptech/server/service/UserService.java
+++ server/src/main/java/school/sptech/server/service/UserService.java
@@ -15,8 +15,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static org.apache.commons.lang3.StringUtils.join;
-
 @Service
 public class UserService {
 
@@ -46,23 +44,24 @@ public class UserService {
 
     }
 
-
-    public  String encriptPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        MessageDigest messageDigest =  MessageDigest.getInstance("SHA-256");
+    public String encriptPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         messageDigest.update(password.getBytes("UTF-8"));
         return new BigInteger(1, messageDigest.digest()).toString(16);
     }
 
-
-    public User loginUser(String password,String email) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public User login(String email, String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 
         password = encriptPassword(password);
 
-
-        return dbUserRepository.findUserByEmailAndPassword(email,password);
+        return dbUserRepository.findByEmailAndPassword(email, password);
 
     }
+    public boolean existsByEmail(String email) {
 
+        return dbUserRepository.existsByEmail(email);
+
+    }
 
     public List<User> getAllWorkers() {
         return dbUserRepository
