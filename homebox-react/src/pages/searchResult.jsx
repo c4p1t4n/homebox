46e0fb25e08ddbc3e4aedfda28fb15adca90c7c3
nikolas-virionis <nikolas.virionis@sptech.school>
2022-05-30 22:24:42 -0300

    enviar requisição para o back para cada pesquisa feita

--- homebox-react/src/pages/searchResult.jsx
+++ homebox-react/src/pages/searchResult.jsx
@@ -5,21 +5,36 @@ import Header from "../components/header"
 import imgJose from "../assets/img/joseRicardoCustumer.png"
 import {useEffect, useState} from "react"
 import {search} from "../assets/js/search"
+import api from "../api"
 
 function SearchResult() {
     const queryString = window.location.search
     const parameters = new URLSearchParams(queryString)
     const searchValue = parameters.get("search")
+    const {id_user} = JSON.parse(sessionStorage.getItem("user"))
 
     const [searchResult, setSearchResult] = useState([])
 
     useEffect(() => {
+        if (id_user) {
+            api.post("/search", {idUser: id_user, value: searchValue}).then(
+                response => {
+                    if (response.status === 201) {
+                        console.log("SUCESSO")
+                    } else {
+                        console.log(response)
+                    }
+                }
+            )
+        }
         search(searchValue)
             .then(value => {
                 console.log(value)
                 setSearchResult(value)
             })
-            .catch(err => console.warn(err))
+            .catch(err => {
+                setSearchResult([])
+            })
     }, [searchValue])
 
     return (
@@ -29,15 +44,17 @@ function SearchResult() {
                 <div className="body">
                     <h2>Exibindo resultado para {`"${searchValue}"`}</h2>
                     <div className="cardCustumerDiv">
-                        {searchResult.map(item => (
-                            <CardSearch
-                                img={item.user?.picture ?? imgJose}
-                                name={item.user?.name}
-                                category={item.user?.category}
-                                ranking={item.ranking ?? "N/A"}
-                                dist={item.distance ?? "N/A"}
-                            />
-                        ))}
+                        {searchResult
+                            ? searchResult.map(item => (
+                                  <CardSearch
+                                      img={item.user?.picture ?? imgJose}
+                                      name={item.user?.name}
+                                      category={item?.category}
+                                      ranking={item.ranking ?? "N/A"}
+                                      dist={item.distance ?? "N/A"}
+                                  />
+                              ))
+                            : ""}
                     </div>
                 </div>
             </div>
