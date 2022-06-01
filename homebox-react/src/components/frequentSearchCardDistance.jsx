5c8f3aa53cbd1d60b6bef6c0b28b981c0abaa54a
nikolas-virionis <nikolas.virionis@sptech.school>
2022-06-01 20:19:47 -0300

    correção nos props dos cards

--- homebox-react/src/components/frequentSearchCardDistance.jsx
+++ homebox-react/src/components/frequentSearchCardDistance.jsx
@@ -1,19 +1,24 @@
 function FrequentSearchCard(props) {
+    const styleRating = {
+        display: "inline"
+    }
+
     return (
-    <div className={props.class}>
-        <p>{props.name}</p>
-        <img className="imgCustumer" src={props.img} alt="Foto de perfil"/>
-        <p>{props.category}</p>
-        <div className="avaliacao">
-            <p>Avaliação:</p>
-            <p>{props.stars}/5</p>
+        <div className={props.class}>
+            <p>{props.name}</p>
+            <img className="imgCustumer" src={props.img} alt="Foto de perfil" />
+            <p>{props.category}</p>
+            <div className="avaliacao">
+                <p style={styleRating}>Avaliação: {props.ranking}/5</p>
+            </div>
+            <img
+                className="stars"
+                src={props.starImg}
+                alt="Estrelas que indicam a avaliação do prestador"
+            />
+            <p>{props.dist.toFixed(2)} km de distância</p>
         </div>
-        <img className="stars" src={props.starImg}
-            alt="Estrelas que indicam a avaliação do prestador"/>
-            <p>{props.dist}</p>
-    </div>
-    );
-  }
-  
-  export default FrequentSearchCard;
-  
+    )
+}
+
+export default FrequentSearchCard
