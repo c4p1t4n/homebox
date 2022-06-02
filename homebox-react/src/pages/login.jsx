e03c592c6c4489df5d88de7d96f6a76838780b65
Rodrigo Garcez <rodrigo.hermann@sptech.school>
2022-06-02 11:39:46 -0300

    Arrumando alerta do login

--- homebox-react/src/pages/login.jsx
+++ homebox-react/src/pages/login.jsx
@@ -10,64 +10,64 @@ import VLibras from "@djpfs/react-vlibras"
 function Login() {
     return (
         <>
-        <VLibras/>
-        <body className="login_body">
-            <div className="homebox_logo">
-                <a href="http://127.0.0.1:5500/website/index.html">
-                    <img className="logo_homebox" src={logo} alt="" />
-                </a>
-            </div>
-            <div className="div_login_and_button">
-                <div className="div_login">
-                    <h2>Login</h2>
-                    <div className="div_emails">
-                        <p>E-mail</p>
-                        <input
-                            onKeyDown={keyStroke}
-                            id="email"
-                            type="e-mail"
-                            placeholder="xxxxxx.xxxxxx@gmail.com.br"
-                        />
-                    </div>
-                    <div className="div_password">
-                        <p>Senha</p>
-                        <input
-                            onKeyDown={keyStroke}
-                            id="password"
-                            type="password"
-                            placeholder="XXXXXXXXXX"
-                        />
-                    </div>
-                    <p>OU</p>
-                    <div className="container_img">
-                        <img src={google} alt="Google" />
-                        <img src={facebook} alt="Facebook" />
-                        <img src={twitter} alt="Twiter" />
-                    </div>
-                </div>
-                <div className="div_button">
-                    <button id="buttonLogin" onClick={login}>Entrar</button>
-                    <br />
-                    <a href="/register">
-                        <p>
-                            Ou <p id="register">cadastre-se</p> aqui
-                        </p>
-                    </a>
-                    <br />
-                    <a href="/">
-                        <p id="password">Esqueceu sua senha ?</p>
+            <VLibras />
+            <body className="login_body">
+                <div className="homebox_logo">
+                    <a href="http://127.0.0.1:5500/website/index.html">
+                        <img className="logo_homebox" src={logo} alt="" />
                     </a>
                 </div>
-            </div>
-        </body>
+                <div className="div_login_and_button">
+                    <div className="div_login">
+                        <h2>Login</h2>
+                        <div className="div_emails">
+                            <p>E-mail</p>
+                            <input
+                                onKeyDown={keyStroke}
+                                id="email"
+                                type="e-mail"
+                                placeholder="xxxxxx.xxxxxx@gmail.com.br"
+                            />
+                        </div>
+                        <div className="div_password">
+                            <p>Senha</p>
+                            <input
+                                onKeyDown={keyStroke}
+                                id="password"
+                                type="password"
+                                placeholder="XXXXXXXXXX"
+                            />
+                        </div>
+                        <p>OU</p>
+                        <div className="container_img">
+                            <img src={google} alt="Google" />
+                            <img src={facebook} alt="Facebook" />
+                            <img src={twitter} alt="Twiter" />
+                        </div>
+                    </div>
+                    <div className="div_button">
+                        <button id="buttonLogin" onClick={login}>Entrar</button>
+                        <br />
+                        <a href="/register">
+                            <p>
+                                Ou <p id="register">cadastre-se</p> aqui
+                            </p>
+                        </a>
+                        <br />
+                        <a href="/">
+                            <p id="password">Esqueceu sua senha ?</p>
+                        </a>
+                    </div>
+                </div>
+            </body>
         </>
     )
 }
 
 const keyStroke = e => {
     const buttonLogin = document.querySelector("#buttonLogin")
-   
-   
+
+
     if (e.key === "Enter") {
         buttonLogin.click()
     }
@@ -96,14 +96,14 @@ const login = e => {
                 console.log("SUCESSO")
                 sessionStorage.setItem(
                     "user",
-                    JSON.stringify({...response.data})
+                    JSON.stringify({ ...response.data })
                 )
                 window.location.href = "/"
             } else if (response.status === 400) {
-                window.alert("Usuario ou Senha errado(s)!")
+                window.alert("Senha errada!")
             } else if (response.status === 404) {
                 console.log("DEU MUITO RUIM")
-                window.alert("Usuario ou Senha errado(s)!")
+                window.alert("Email não encontrado!")
 
             }
         })
