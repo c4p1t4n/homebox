35d8e67da6a166bd349e199fc6c69c886d724416
Rodrigo Garcez <rodrigo.hermann@bandtec.com.br>
2022-08-18 21:38:21 -0300

    Chat pronto para back-end

---
+++ homebox-react/src/components/cardChatMsgRight.jsx
@@ -0,0 +1,11 @@
+function cardChatMsgRight() {
+    return (
+        <>
+            <div className="cardChatMsgRight">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sunt quam in  accusantium delectus.</p>
+            </div>
+        </>
+    )
+}
+
+export default cardChatMsgRight
