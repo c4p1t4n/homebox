bcc0d8b36569f88115c196b3da479198a9d57785
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2022-08-26 23:42:25 -0300

    Implementando a função de audio

--- homebox-react/src/pages/chat.jsx
+++ homebox-react/src/pages/chat.jsx
@@ -4,9 +4,11 @@ import CardChat from "../components/cardChat";
 import CardChatMsgLeft from "../components/cardChatMsgLeft";
 import CardChatMsgRight from "../components/cardChatMsgRight";
 
-
 import VLibras from "@djpfs/react-vlibras"
-
+import api from "../api"
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+import { v4 as uuidv4 } from 'uuid';
+import React, { Component } from 'react'
 
 import iconBack from "../assets/img/arrowLeft.png"
 import iconSendMsg from "../assets/img/iconSendMsg.png"
@@ -15,59 +17,120 @@ import iconSendMp3 from "../assets/img/iconSendMp3.png"
 import iconHomebox from "../assets/img/icon/logo-removebg-preview.png"
 import searchIcon from "../assets/img/searchIconBlack.png"
 
+class Chat extends Component {
+    constructor(props) {
+        super(props)
 
-function Chat() {
-    return (
-        <>
-            <VLibras />
-            <div className="bodyChat">
-                <div className="divLeft">
-                    <div className="divLogoChat">
-                        <button className="butIconBack"><img className="iconBack" src={iconBack} alt="voltar" /></button>
-                        <img className="iconHomebox" src={iconHomebox} alt="logo homebox" />
-                    </div>
-                    <div className="convs">
-                        <h5>
-                            Conversas
-                        </h5>
-                    </div>
-                    <div className="divSearchBar">
-                        <div className="barSearch">
-                            <input placeholder="Pesquisar..." type="text" />
+        this.state = {
+            recordState: null
+        };
+    }
+
+        start = () => {
+            this.setState({
+                recordState: RecordState.START
+            });
+        };
+
+        stop = () => {
+            this.setState({
+                recordState: RecordState.STOP
+            });
+        };
+
+        onStop = (audioData) => {
+            console.log(typeof audioData);
+            console.log(audioData);
+
+            console.log(typeof audioData.blob);
+            console.log(audioData.blob);
+
+
+            // var blob = new Blob()
+            // console.log(typeof blob);
+            // console.log(blob);
+            
+            var file = new File([audioData.blob], "file",{lastModified: new Date().getTime(),
+                type: audioData.type });
+            console.log(file);
+            
+            const url = '/upload/uploadAudio';
+            const formData = new FormData();
+            // formData.append("file", file, uuidv4()+".mp3");
+            formData.append("file", audioData.blob, uuidv4()+".mp3");
+            const config = {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            }
+
+            api.post(url, formData, config)
+                .then((response) => {
+                    console.log(response.status)
+                })
+        };
+
+        render (){
+            const { recordState } = this.state;
+            return(
+            <>
+                <VLibras />
+                <div className="bodyChat">
+                    <div className="divLeft">
+                        <div className="divLogoChat">
+                            <button className="butIconBack"><img className="iconBack" src={iconBack} alt="voltar" /></button>
+                            <img className="iconHomebox" src={iconHomebox} alt="logo homebox" />
                         </div>
-                        <div className="iconSearch">
-                            <button><img src={searchIcon} alt="Icone de pesquisar 'lupa'" /></button>
+                        <div className="convs">
+                            <h5>
+                                Conversas
+                            </h5>
+                        </div>
+                        <div className="divSearchBar">
+                            <div className="barSearch">
+                                <input placeholder="Pesquisar..." type="text" />
+                            </div>
+                            <div className="iconSearch">
+                                <button><img src={searchIcon} alt="Icone de pesquisar 'lupa'" /></button>
+                            </div>
+                        </div>
+                        <div className="divCardsChat">
+                            <CardChat />
+                            <CardChat />
+                            <CardChat />
+                            <CardChat />
+                            <CardChat />
                         </div>
                     </div>
-                    <div className="divCardsChat">
-                        <CardChat />
-                        <CardChat />
-                        <CardChat />
-                        <CardChat />
-                        <CardChat />
+                    <div className="divRight">
+                        <div className="topChat">
+                            <div className="overflowDiv">
+                                <CardChatMsgLeft />
+                                <CardChatMsgLeft />
+                                <CardChatMsgLeft />
+                                <CardChatMsgRight />
+                                <CardChatMsgRight />
+                                <CardChatMsgRight />
+                                <div>
+                                    <AudioReactRecorder state={recordState} onStop={this.onStop}/>
+
+                                    <button onClick={this.start}>Start</button>
+                                    <button onClick={this.stop}>Stop</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="butChat">
+                            <input placeholder="Digite aqui ..." type="text" className="msg" />
+                            <button><img src={iconSendMsg} alt="Icone para enviar mensagem" className="sendMsg" /></button>
+                            <button><img src={paperclip} alt="Icone para anexar foto ou video" className="anexImg" /></button>
+                            <button><img src={iconSendMp3} alt="Icone para enviar audio" className="iconMic" /></button>
+
+                        </div>
                     </div>
                 </div>
-                <div className="divRight">
-                    <div className="topChat">
-                        <div className="overflowDiv">
-                            <CardChatMsgLeft />
-                            <CardChatMsgLeft />
-                            <CardChatMsgLeft />
-                            <CardChatMsgRight />
-                            <CardChatMsgRight />
-                            <CardChatMsgRight />
-                        </div>
-                    </div>
-                    <div className="butChat">
-                        <input placeholder="Digite aqui ..." type="text" className="msg" />
-                        <button><img src={iconSendMsg} alt="Icone para enviar mensagem" className="sendMsg" /></button>
-                        <button><img src={paperclip} alt="Icone para anexar foto ou video" className="anexImg" /></button>
-                        <button><img src={iconSendMp3} alt="Icone para enviar audio" className="iconMic" /></button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }    
 }
 
 export default Chat
