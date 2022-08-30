29fd08d4f2653bb4d793992fa5398471b6648b1f
Rodrigo Garcez <rodrigo.hermann@bandtec.com.br>
2022-08-30 20:51:56 -0300

    Uma parte do relatorio

--- homebox-react/src/pages/relatorioProvider.jsx
+++ homebox-react/src/pages/relatorioProvider.jsx
@@ -1,5 +1,9 @@
 import MenuLeftProvider from "../components/menuLeftProvider"
 
+import "../assets/css/providerDash.css"
+
+import questionMark from "../assets/img/quesntion-mark.png"
+
 
 function relatorioProvider() {
     return (
@@ -13,20 +17,20 @@ function relatorioProvider() {
                                 <div className="visitsYourProfile">
                                     <div className="divPTop">
                                         <p>Visitas ao seu perfil</p>
-                                        <img src="" alt="informação sobre visitas ao seu perfil" />
+                                        <img src={questionMark} alt="informação sobre visitas ao seu perfil" />
                                     </div>
                                     <div className="divPbutt">
-                                        <p>+13%</p>
+                                        <p id="indice">+13%</p>
                                         <p>24 <br />nessa semana</p>
                                     </div>
                                 </div>
                                 <div className="yourAvarage">
                                     <div className="divPTop">
                                         <p>Sua nota (média)</p>
-                                        <img src="" alt="informação sobre sua média" />
+                                        <img src={questionMark} alt="informação sobre sua média" />
                                     </div>
                                     <div className="divPbutt">
-                                        <p>+0.10</p>
+                                        <p id="indice">+0.10</p>
                                         <div className="avarageProviderDash">
                                             <p>4.2 <br />XXXXXX</p>
                                         </div>
