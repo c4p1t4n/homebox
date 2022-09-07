ee122effe984c0552ecc825c3c5f2b9ff55e1a11
Leo <leoigornunes@protonmail.com>
2022-09-07 16:26:32 -0300

    select que pega o interestAcess dos ultimos 7 dias

--- server/src/main/java/school/sptech/server/repository/InterestAccessRepository.java
+++ server/src/main/java/school/sptech/server/repository/InterestAccessRepository.java
@@ -1,8 +1,13 @@
 package school.sptech.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import school.sptech.server.model.InterestAccess;
 
 public interface InterestAccessRepository extends JpaRepository<InterestAccess, Integer> {
+
+
+    @Query(value="select COUNT(ia) from school.sptech.server.model.InterestAccess ia where ia.user.id = ?1 and  ia.accessDate >= DATE(CURRENT_DATE)")
+    Integer countIntestAcess(Integer id_user);
 
 }
