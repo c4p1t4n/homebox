95400b800b8f09765f1f01e646328dad284c01db
Leo <leoigornunes@protonmail.com>
2022-09-07 22:41:02 -0300

    Querys para buscar os dados necessarios no frontend

--- server/src/main/java/school/sptech/server/repository/RatingRepository.java
+++ server/src/main/java/school/sptech/server/repository/RatingRepository.java
@@ -4,11 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import school.sptech.server.model.Rating;
+import school.sptech.server.response.RatingDateLastSevenDays;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
     @Query("select AVG(rat.rating) from Rating rat where rat.accomplishedService.scheduling.service.worker.id = ?1 GROUP BY rat.accomplishedService.scheduling.service.worker.id")
     Double getAvgRatingForWorker(Integer idWorker);
+
+    @Query("select  AVG(rat.rating) from Rating rat where rat.accomplishedService.scheduling.service.worker.id = ?1  and rat.accomplishedService.serviceDate  BETWEEN  DATE(?2)  and DATE(CURRENT_DATE)  GROUP BY rat.accomplishedService.scheduling.service.worker.id,rat.accomplishedService.serviceDate ORDER BY  rat.accomplishedService.serviceDate ")
+    List<Double> getAvgRatingForWorkerLastSevenDays(Integer idWorker,LocalDate date);
+
 
     boolean existsByAccomplishedServiceSchedulingIdScheduling(Integer id);
 
