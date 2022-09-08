7fba50e219d32f549ad46a4ebe08c931f3c30eb6
Rodrigo Garcez <rodrigo.hermann@bandtec.com.br>
2022-09-08 16:00:12 -0300

    commit

--- homebox-react/src/pages/profileProvider.jsx
+++ homebox-react/src/pages/profileProvider.jsx
@@ -50,63 +50,6 @@ function profileProvider() {
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eum. Adipisci, deserunt reprehenderit quaerat suscipit enim, delectus voluptatibus modi autem facilis dele</p>
                             </div>
                         </details>
-                        <details className="cardServiceProvider">
-                            <summary className="infoService">
-                                <div className="nameServiceProvider">
-                                    <p>Serviço:</p>
-                                    <p>xxxxxxx</p>
-                                </div>
-                                <div className="valueAvg">
-                                    <p>Valor de Referência</p>
-                                    <p>R$00,00</p>
-                                </div>
-                                <div className="divIconsService">
-                                    <img src={alterNameProvider} alt="icone para editr serviço" className="editServiceProvider" />
-                                    <img src={deleteService} alt="icone de deletar serviço" className="deleteService" />
-                                </div>
-                            </summary>
-                            <div className="descriptionServiceProvider">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eum. Adipisci, deserunt reprehenderit quaerat suscipit enim, delectus voluptatibus modi autem facilis dele</p>
-                            </div>
-                        </details>
-                        <details className="cardServiceProvider">
-                            <summary className="infoService">
-                                <div className="nameServiceProvider">
-                                    <p>Serviço:</p>
-                                    <p>xxxxxxx</p>
-                                </div>
-                                <div className="valueAvg">
-                                    <p>Valor de Referência</p>
-                                    <p>R$00,00</p>
-                                </div>
-                                <div className="divIconsService">
-                                    <img src={alterNameProvider} alt="icone para editr serviço" className="editServiceProvider" />
-                                    <img src={deleteService} alt="icone de deletar serviço" className="deleteService" />
-                                </div>
-                            </summary>
-                            <div className="descriptionServiceProvider">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eum. Adipisci, deserunt reprehenderit quaerat suscipit enim, delectus voluptatibus modi autem facilis dele</p>
-                            </div>
-                        </details>
-                        <details className="cardServiceProvider">
-                            <summary className="infoService">
-                                <div className="nameServiceProvider">
-                                    <p>Serviço:</p>
-                                    <p>xxxxxxx</p>
-                                </div>
-                                <div className="valueAvg">
-                                    <p>Valor de Referência</p>
-                                    <p>R$00,00</p>
-                                </div>
-                                <div className="divIconsService">
-                                    <img src={alterNameProvider} alt="icone para editr serviço" className="editServiceProvider" />
-                                    <img src={deleteService} alt="icone de deletar serviço" className="deleteService" />
-                                </div>
-                            </summary>
-                            <div className="descriptionServiceProvider">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eum. Adipisci, deserunt reprehenderit quaerat suscipit enim, delectus voluptatibus modi autem facilis dele</p>
-                            </div>
-                        </details>
                     </div>
                 </div>
             </div>
