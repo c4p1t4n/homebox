3c4f990f2fc6273e6a41b653d30ddafbc4bd2917
nikolas-virionis <nikolas.virionis@sptech.school>
2022-09-29 21:22:42 -0300

    alteração para bloquear cors tentativa 1

--- homebox-react/src/api.js
+++ homebox-react/src/api.js
@@ -3,5 +3,6 @@ import axios from "axios"
 const api = axios.create({
     baseURL: "http://10.0.0.39:8080/"
 })
+api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export default api
