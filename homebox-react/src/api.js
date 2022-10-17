81ed320295d5a1507c9fb60dba52a7607e951a1c
leo.oliveira <leo.oliveira@yhub.com.br>
2022-10-17 18:48:38 -0300

    Arrumando apontamento

--- homebox-react/src/api.js
+++ homebox-react/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    // baseURL: "http://54.198.76.123:8080/"
-    baseURL: "http://localhost:8080/"
+    baseURL: "http://54.198.76.123:8080/"
+    // baseURL: "http://localhost:8080/"
 })
 api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
