8eebb637d51f0e50e6ce39fb232d254f31d83896
Rodrigo Garcez <rodrigo.hermann@bandtec.com.br>
2022-10-17 18:03:25 -0300

    Arrumando o update servie e delete service

--- homebox-react/src/pages/profileProvider.jsx
+++ homebox-react/src/pages/profileProvider.jsx
@@ -45,6 +45,7 @@ function profileProvider() {
             referencePrice: document.getElementById("editServiceDivRefValueId").value
         }).then((response) => {
             console.log(response.status)
+            window.location.reload()
         })
 
         document.getElementById("editServiceDiv").style.display = "none"
@@ -224,6 +225,7 @@ function closeDeleteService() {
 function deleteService() {
     api.delete(`/services/delete/` + JSON.parse(sessionStorage.getItem("service")).idService).then((response) => {
         console.log(response.status)
+        window.location.reload()
     })
 
     document.getElementById("deleteServiceProvider").style.display = "none"
