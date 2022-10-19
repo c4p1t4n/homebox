7c1bb368b76d147f107e705fe8816820800ba2d5
Rodrigo Garcez <rodrigo.hermann@bandtec.com.br>
2022-10-19 17:57:55 -0300

    Alteradno imagem da larissa

--- database/homebox.sql
+++ database/homebox.sql
@@ -237,7 +237,7 @@ VALUES (
         '12345678900',
         NULL,
         'worker',
-        'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1961&q=80',
+        'https://images.unsplash.com/photo-1601934066701-718da32cd34d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
         DATE('1989-01-01'),
         '03314030',
         'y'
