3942047daa9200d0c372e3e2d8f34ed7093fe51f
Rodrigo Garcez <rodrigo.hermann@bandtec.com.br>
2022-11-28 19:57:28 -0300

    Alterando para novo back-end no outro lab

--- homebox-react/src/api.js
+++ homebox-react/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://54.198.76.123:8080/"
+    baseURL: "http://3.218.87.61:8080/"
     // baseURL: "http://localhost:8000/"
 })
 api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
